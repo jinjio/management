6ac0c5c7f7410fcbcd1bafc83e9e94250da5c793
jinjio <35422448+jinjio@users.noreply.github.com>
2018-02-09 19:55:44 +0900

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,17 @@
+<?php
+$con = mysqli_connect("localhost", "", "", "");
+
+	$userID = $_POST["userID"];
+	$userPassword = $_POST["userPassword"];
+	$userName = $_POST["userName"];
+	$userAge = $_POST["userAge"];
+	
+	$statement = mysqli_prepare($con, "INSERT INTO USER VALUES (?,?,?,?)");
+	mysqli_stmt_bind_param($statement, "sssi", $uesrID, $userPassword, $userName, $userAge);
+	mysqli_stmt_execute($statement);
+
+	$response = array();
+	$response["success"] = true;
+
+	echo json_encode($response);
+?>
